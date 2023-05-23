b3752f6b3e5701a466d72b90af9b3d8d422c7d16
Aleksey Pechurin <pe4urin@yandex.ru>
2023-05-23 18:34:37 +0300

    добавлен поиск по товарам

--- src/constants.js
+++ src/constants.js
@@ -6,3 +6,19 @@ export const CART_URL = 'https://646382e64dca1a661360142a.mockapi.io/cart'
 export const FAVORITES_URL = 'https://64677a982ea3cae8dc3038dc.mockapi.io/favorites'
 
 export const ORDERS_URL = 'https://64677a982ea3cae8dc3038dc.mockapi.io/orders'
+
+
+export const categories = [
+  {
+    name: "Все",
+    value: "all",
+  },
+  {
+    name: "Мужские",
+    value: "men",
+  },
+  {
+    name: "Женские",
+    value: "women",
+  },
+];
--- src/pages/TheCatalog.vue
+++ src/pages/TheCatalog.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="main-wrapper">
     <div class="cards-header">
-      <h1 class="page-title">Все кроссовки</h1>
+      <h1 class="page-title">
+        {{ isSearch ? `Поиск по запросу:` : "Все кроссовки" }}
+      </h1>
       <BaseSelect
         class="select"
         :selected="selected"
@@ -10,8 +12,23 @@
       />
       <div class="search">
         <BaseIcon name="search" />
-        <input type="text" placeholder="Поиск" />
+        <input
+          type="text"
+          placeholder="Поиск"
+          v-model="searchValue"
+          @keydown.enter="search(searchValue)"
+        />
       </div>
+      <p class="search-result" v-if="isSearch">
+        {{ searchResult }}
+        <button
+          class="icon-btn close-btn"
+          type="button"
+          @click="resetSearchResult"
+        >
+          <BaseIcon name="remove" />
+        </button>
+      </p>
     </div>
     <ul class="cards" v-if="!isLoading">
       <TheCard
@@ -29,31 +46,23 @@ import TheCard from "@/components/TheCard";
 import BaseSelect from "@/components/UI/BaseSelect.vue";
 import BaseIcon from "@/components/UI/BaseIcon.vue";
 import BaseLoader from "@/components/UI/BaseLoader.vue";
+import { categories } from "@/constants";
 import { useStore } from "vuex";
 import { onMounted, computed, ref } from "vue";
 
 const store = useStore();
 
+// product
+
 const getProducts = () => store.dispatch("GET_PRODUCTS");
 onMounted(getProducts);
 
 const products = computed(() => store.getters.PRODUCTS);
+
 const isLoading = computed(() => store.state.isLoading);
 
-const categories = [
-  {
-    name: "Все",
-    value: "all",
-  },
-  {
-    name: "Мужские",
-    value: "men",
-  },
-  {
-    name: "Женские",
-    value: "women",
-  },
-];
+// filter
+
 const selected = ref("Категории");
 const sortedProducts = ref([]);
 const filteredProducts = computed(() =>
@@ -68,17 +77,58 @@ const sortByCategories = (category) => {
   });
   selected.value = category.name;
 };
+
+// search
+
+const searchValue = ref("");
+const searchResult = ref("");
+const isSearch = ref(false);
+
+const search = (value) => {
+  sortedProducts.value = filteredProducts.value.filter((item) =>
+    item.title.toLowerCase().includes(value.toLowerCase())
+  );
+  searchResult.value = searchValue.value;
+  searchValue.value = "";
+  isSearch.value = true;
+};
+
+const resetSearchResult = () => {
+  isSearch.value = false;
+  sortedProducts.value = products.value;
+  searchResult.value = "";
+};
 </script>
 
 <style lang="scss">
 .cards-header {
-  margin-bottom: 40px;
+  position: relative;
+  padding-bottom: 40px;
+  margin-bottom: 20px;
 
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
+.search-result {
+  position: absolute;
+  left: 20px;
+  bottom: 0;
 
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 6px;
+
+  border: 2px solid #dadada;
+  border-radius: 6px;
+
+  & button {
+    height: 20px;
+    width: 20px;
+    margin-left: 10px;
+  }
+}
 .select {
   margin-left: auto;
   margin-right: 10px;
@@ -92,7 +142,7 @@ const sortByCategories = (category) => {
   border: 1px solid #f3f3f3;
   border-radius: 10px;
   & input {
-    width: 200px;
+    width: 100%;
     padding: 10px;
 
     font-family: inherit;
@@ -103,11 +153,18 @@ const sortByCategories = (category) => {
     outline: none;
   }
 }
-
+@media (max-width: 1024px) {
+  .search {
+    width: 200px;
+  }
+}
 @media (max-width: 768px) {
   .cards-header {
     flex-direction: column;
-    row-gap: 20px;
+    row-gap: 10px;
+    & h1 {
+      order: 2;
+    }
   }
   .select {
     order: 1;
